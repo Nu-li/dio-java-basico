adc3465ecdbe6fa1c43e8fe25071f0ba790bd74f
nuli <develid.nechylayla@gmail.com>
2023-10-05 12:50:29 -0300

    #

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,30 @@
+public class Operadores {
+    public static void main(String[] args) {
+        /* EXEMPLO DE ADIÇÃO +1
+        int numero = 5;
+        // x repetição
+        numero++ ;
+        //numero = numero + 1;
+        System.out.println( ++ numero);
+        System.out.println(numero);*/
+
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        /* EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+        if(a==b)
+        resultado = "verdadeiro";
+        else
+        resultado = "falso";
+        */
+
+        //MESMA CONDICIONAL, MAS COM OPERADOR CONDICIONAL TERNÁRIO
+        String resultado = (a==b) ? "verdadeiro" : "false";
+
+        System.out.println(resultado);
+    }
+
+   
+}
